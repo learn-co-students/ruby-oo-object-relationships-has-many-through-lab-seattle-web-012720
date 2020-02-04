2879f151983bfd90fb0aaa6a3fcd62dafae4c334
Jricecake <jonathanriecke@gmail.com>
2020-02-03 17:42:58 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@all = []
+
+    def initialize(title, artist, genre)
+        @title = title
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
